a83c0ee46d138e176b88b512f0fb957beee9dbe0
Jonathan Williams <jprdesign@gmail.com>
2018-01-03 21:45:04 +0800

    Make functions return modified structs

--- lib/exmachina/neuron.ex
+++ lib/exmachina/neuron.ex
@@ -18,20 +18,26 @@ defmodule Exmachina.Neuron do
   defstruct soma: nil, dendrites: nil
 
   defmodule Soma do
-    defstruct input_activities: %{}, num_inputs: nil, last_activity: nil, bias: nil
+    defstruct input_activities: %{}, num_inputs: nil, last_activity: nil, bias: nil, activity: nil
 
-    def compute_activity(%Soma{input_activities: input_activities, bias: bias}) do
-      sum_activity = (Map.values(input_activities) ++ [bias]) |> Enum.sum
-      Numerix.Special.logistic(sum_activity)
+    def add_input_activity(%Soma{input_activities: input_activities} = soma, activity, from) do
+      %{soma | input_activities: Map.put(input_activities, from, activity)}
     end
 
-    def reply_with_error(%Soma{input_activities: input_activities}, errors, activity) do
+    def compute_activity(%Soma{input_activities: input_activities, bias: bias} = soma) do
+      sum_activity = (Map.values(input_activities) ++ [bias]) |> Enum.sum
+      activity = Numerix.Special.logistic(sum_activity)
+      %{soma | activity: activity}
+    end
+
+    def reply_with_error(%Soma{input_activities: input_activities, activity: activity} = soma, errors) do
       global_error = calculate_global_error(errors)
       input_error  = calculate_error_for_inputs(activity, global_error)
 
       Enum.all?(input_activities, fn {input_pid, _activity} ->
         :ok == GenServer.reply(input_pid, input_error)
       end)
+      %{soma | input_activities: %{}}
     end
 
     defp calculate_global_error(errors) do
@@ -48,25 +54,30 @@ defmodule Exmachina.Neuron do
   defmodule Dendrites do
     @learning_rate 3.0
 
-    defstruct output_weights: %{}
+    defstruct output_weights: %{}, errors: %{}
 
-    def output_activity(%Dendrites{output_weights: output_weights}, activity) do
-      Task.async_stream(output_weights, fn {output_pid, weight} ->
-        weighted_activity = calculate_weighted_activity(activity, weight)
-        error = Exmachina.Neuron.activate(output_pid, weighted_activity)
-        {output_pid, {error, weight}}
-      end, max_concurrency: 999)
-      |> Enum.map(fn {:ok, val} -> val end)
-      |> Enum.into(%{})
+    def output_activity(%Dendrites{output_weights: output_weights} = dendrites, activity) do
+      errors = output_weights
+        |> Task.async_stream(fn {output_pid, weight} ->
+          weighted_activity = calculate_weighted_activity(activity, weight)
+          error = Exmachina.Neuron.activate(output_pid, weighted_activity)
+          {output_pid, {error, weight}}
+        end, max_concurrency: 999)
+        |> Enum.map(fn {:ok, val} -> val end)
+        |> Enum.into(%{})
+
+      %{dendrites | errors: errors}
     end
 
-    def adjust_weights(%Dendrites{}, errors, activity) do
-      Enum.map(errors, fn {output_pid, {error, weight}} ->
-        new_weight = weight - error * activity * @learning_rate
+    def adjust_weights(%Dendrites{errors: errors} = dendrites, activity) do
+      new_weights = errors
+        |> Enum.map(fn {output_pid, {error, weight}} ->
+          new_weight = weight - error * activity * @learning_rate
+          {output_pid, new_weight}
+        end)
+        |> Enum.into(%{})
 
-        {output_pid, new_weight}
-      end)
-      |> Enum.into(%{})
+      %{dendrites | output_weights: new_weights}
     end
 
     defp calculate_weighted_activity(activity, weight) do
@@ -76,7 +87,7 @@ defmodule Exmachina.Neuron do
 
   def start_link(num_inputs: num_inputs, output_pids: output_pids) do
     bias = init_weight() - 2.0
-    soma = %Soma{num_inputs: num_inputs, bias: init_weight() - 2.0}
+    soma = %Soma{num_inputs: num_inputs, bias: bias}
 
     output_weights = output_pids |> Enum.map(& {&1, init_weight()}) |> Enum.into(%{})
     dendrites = %Dendrites{output_weights: output_weights}
@@ -88,10 +99,10 @@ defmodule Exmachina.Neuron do
   def get_weight_for(pid, output_pid), do: GenServer.call(pid, {:get_weight_for, output_pid})
 
   def handle_call({:activate, activity}, from, %__MODULE__{soma: soma} = state) do
-    new_activities = add_input_activity(activity, from, soma.input_activities)
-    fire_if_all_received(new_activities, soma.num_inputs)
+    soma = Soma.add_input_activity(soma, activity, from)
+    fire_if_all_received(soma.input_activities, soma.num_inputs)
 
-    {:noreply, %{state | soma: %{soma | input_activities: new_activities}}}
+    {:noreply, %{state | soma: soma}}
   end
 
   def handle_call({:get_weight_for, output_pid}, _from, %__MODULE__{dendrites: dendrites} = state) do
@@ -99,18 +110,14 @@ defmodule Exmachina.Neuron do
   end
 
   def handle_cast(:fire, %__MODULE__{dendrites: dendrites, soma: soma} = state) do
-    with activity    <- Soma.compute_activity(soma),
-         errors      <- Dendrites.output_activity(dendrites, activity),
-         true        <- Soma.reply_with_error(soma, errors, activity),
-         new_weights <- Dendrites.adjust_weights(dendrites, errors, activity),
-    do:  {:noreply, %{state | soma: %{soma | input_activities: %{}}, dendrites: %{dendrites | output_weights: new_weights}}}
+    with soma        <- Soma.compute_activity(soma),
+         dendrites   <- Dendrites.output_activity(dendrites, soma.activity),
+         soma        <- Soma.reply_with_error(soma, dendrites.errors),
+         dendrites   <- Dendrites.adjust_weights(dendrites, soma.activity),
+    do:  {:noreply, %{state | soma: soma, dendrites: dendrites}}
   end
 
   defp init_weight, do: (:rand.uniform() * 2) - 1.0
-
-  defp add_input_activity(activity, from, input_activities) do
-    Map.put(input_activities, from, activity)
-  end
 
   defp fire_if_all_received(input_activities, num_inputs) do
     if map_size(input_activities) == num_inputs, do: GenServer.cast(self(), :fire)
